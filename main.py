27b3c9063ea8c0e7a5611c28a4928ba1d5af5447
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2022-08-05 12:52:59 +0600

    Fix table parsing and max/min

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import csv
+
 import requests
 from bs4 import BeautifulSoup
 
 list = ['Title', 'Price', 'Stock', 'Link', 'UPC', 'Product Type', 'Price', 'Price with Tax', 'Tax', 'Availability', 'Number of Reviews']
 max_price = 0
-min_price = 0
+min_price = 1000000
 def main():
+    csv_file = open("books_toscrape.csv", "a")
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(list)
+    
     for page_number in range(1, 51):
         url = f"https://books.toscrape.com/catalogue/page-{page_number}.html"
 
 
         source = requests.get(url).text
         soup = BeautifulSoup(source, 'lxml')  # BeautifulSoup constructor
-
-        # csv file
-        csv_file = open("books_toscrape.csv", "a")
-        csv_writer = csv.writer(csv_file)
-
-        if page_number == 1:
-            csv_writer.writerow(list)
 
         books = soup.find_all(class_="product_pod")
 
@@ -33,9 +31,9 @@ def main():
             print(price_float)
 
             if price_float > max_price:
-                price_float = max_price
+                max_price = price_float
             if price_float < min_price:
-                price_float = min_price
+                min_price = price_float
 
 
 
@@ -48,21 +46,17 @@ def main():
             soup = BeautifulSoup(open_link, 'lxml')
             open_book = soup.find(class_="product_page")
 
-            # table in book
+            
+            table = []
             for list1 in range(1, 8):
-                table = open_book.select_one(f'table tr:nth-child({list1})').text.strip()
-                print(table)
-
-            # ipdb.set_trace()
-
-            # table = open_book.find(class_='table table-striped').text
-
-            # book = {"Title": title, "Stock": in_stock, 'Link': url}
-            # print(book)
-
-        csv_writer.writerow(list)
-
-        csv_file.close()
+                table.append(
+                    open_book.select_one(f'table tr:nth-child({list1}) td').text.strip()
+                )
+            csv_writer.writerow([title, price_float, in_stock, link, *table])
+        print('Maximum price is', max_price)
+        print('Minimum price is', min_price)
+    csv_file.close()
+    
 
 print(max_price)
 
